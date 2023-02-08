aa68a6e408864c736c7f1020d29f8aa4f122f4c5
T2LIPthedeveloper <77957349+T2LIPthedeveloper@users.noreply.github.com>
2023-02-08 16:10:02 +0800

    HWFunctions added

---
+++ HWFunctions.java
@@ -0,0 +1,20 @@
+
+
+public class HWFunctions {
+    public static void main(String[] args) {
+        HWFunctions hw = new HWFunctions();
+        System.out.println(hw.isPalindrome("abba"));
+        System.out.println(hw.isPalindrome("abbab"));
+    }
+
+    public  boolean isPalindrome(String s) {
+        if (s.length() == 1 || (s.length() == 2 && s.charAt(0) == s.charAt(1))) {
+            return true;
+        }
+        else if (s.charAt(0) == s.charAt(s.length() - 1)) {
+            return isPalindrome(s.substring(1, s.length()-2));
+        }
+        else
+            return false;
+    }
+}
