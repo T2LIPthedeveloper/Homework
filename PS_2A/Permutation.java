445a098b9704da6bdd473aa8b4e3a14a9cbb54cb
T2LIPthedeveloper <77957349+T2LIPthedeveloper@users.noreply.github.com>
2023-02-22 21:47:38 +0800

    Updated Permutation class.

--- PS_2A/Permutation.java
+++ PS_2A/Permutation.java
@@ -14,8 +14,22 @@ public class Permutation {
 
     public void permute(){
         // produce the permuted sequences of 'in' and store in 'a', recursively
+        permuteHelper("", in);
         
     }
+
+    private void permuteHelper(String start, String end) {
+        if (end.length() <= 1) {
+            a.add(start + end);
+        } else {
+            for (int i = 0; i < end.length(); i++) {
+                String newString = end.substring(0, i) + end.substring(i + 1);
+                permuteHelper(start + end.charAt(i), newString);
+            }
+        }
+    }
+
+
 
     public ArrayList<String> getA(){
         return a;
