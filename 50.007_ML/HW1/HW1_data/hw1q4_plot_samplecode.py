9df8c68196072c9cc045866bd79d22652f986c60
T2LIPthedeveloper <77957349+T2LIPthedeveloper@users.noreply.github.com>
2023-06-09 02:38:40 +0800

    Completed basic ML models for all questions.

--- 50.007_ML/HW1/HW1_data/hw1q4_plot_samplecode.py
+++ 50.007_ML/HW1/HW1_data/hw1q4_plot_samplecode.py
@@ -12,5 +12,6 @@ for	i in index:
     w =  ridgeregression(tX, tY, 10**-i)
     tloss = tloss + [np.sum((np.dot(tX, w) − tY)**2)/tn/2]
     vloss = vloss + [np.sum((np.dot(vX, w) − vY)**2)/vn/2]
+    
 plt.plot(index, np.log(tloss), 'r')
 plt.plot(index, np.log(vloss), 'b')
