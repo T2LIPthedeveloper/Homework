dc6d6a7939687fba2f766555f7aedb16e465e410
T2LIPthedeveloper <77957349+T2LIPthedeveloper@users.noreply.github.com>
2023-07-10 14:22:22 +0800

    First commit for Ce9

--- 50.003_ESC/W9_CE9/ce9_q1/test/findmax.test.js
+++ 50.003_ESC/W9_CE9/ce9_q1/test/findmax.test.js
@@ -3,14 +3,18 @@ const findmax = require('../src/findmax.js');
 describe('testing findmax', () => {
     // TODO: a test that results in failure (do not compute maximum) it should fail
     test('a test that fails', () => {
-        
+        const result = findmax([1,2,3,4,5]);
+        expect(result).toBe(4);
     })
     // TODO: a test that results in error, it should throw an error that the test won't catch
     test('a test that throws error', () => {
+        const result = findmax([]);
+        expect(result).toThrow();
 
     })
     // TODO: a test that results in pass
     test('a test that passes', () => {
-
+        const result = findmax([5,4,3,2,1,6]);
+        expect(result).toBe(6);
     })
 });
